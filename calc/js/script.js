ad763ee0efae1b60238462b9d5690479d60ad8a4
RonOvski1212 <245785gio@gmail.com>
2023-07-26 07:35:10 +0400

    request calc test

--- calc/js/script.js
+++ calc/js/script.js
@@ -9,4 +9,19 @@ inputGel.addEventListener("input", () => {
   request.open("GET", "js/current.json");
   request.setRequestHeader("content-type", "application/json", "charset=utf-8");
   request.send();
+
+  request.addEventListener("load", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const data = JSON.parse(request.response);
+      inputUsd.value = (+inputGel.value / data.current.usd).toFixed(2);
+    } else {
+      inputUsd.value = "დაფიქსირდა ხარვეზი, მოგვიანებით სცადეთ";
+    }
+  });
+
+  //status
+  //statusText
+  //response
+  //readyState
 });
